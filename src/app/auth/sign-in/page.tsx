af14d76d2ba3a549903da5287fff0067161540af
sirax_67 <abrakate20@gmail.com>
2026-06-12 19:29:09 +0300

    sign-in schema

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -13,7 +13,6 @@ export default function SignIn () {
     const schema = z.object({
         email: z.email(),
         password: z.string(),
-        name: z.string(),
     })
 
     const signInMutation = useMutation({
