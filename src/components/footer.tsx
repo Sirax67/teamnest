c7d4ca70e24b2f50b4d8f1624658213d07dc82d1
sirax_67 <abrakate20@gmail.com>
2026-07-10 23:12:09 +0300

    footer mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -18,19 +18,19 @@ export function Footer () {
                         <Link href="/" className="size-8 relative">
                             <Image
                             src="/images/icons/Telegram.svg"
-                            alt="vk"
+                            alt="telegram"
                             fill
                             ></Image>
                         </Link>
                     </div>
-                    <div className="flex gap-6 ">
-                        <Link href="/" className="hover:text-gray-600 lg:text-xl">Партнер</Link>
-                        <Link href="/" className="hover:text-gray-600 lg:text-xl">Партнер</Link>
-                        <Link href="/" className="hover:text-gray-600 lg:text-xl">Партнер</Link>
-                        <Link href="/" className="hover:text-gray-600 lg:text-xl">Партнер</Link>
-                    </div>
+                    <nav className="flex gap-6 ">
+                        <Link href="/" className="hover:text-gray-600 text-xl">Партнер</Link>
+                        <Link href="/" className="hover:text-gray-600 text-xl">Партнер</Link>
+                        <Link href="/" className="hover:text-gray-600 text-xl">Партнер</Link>
+                        <Link href="/" className="hover:text-gray-600 text-xl">Партнер</Link>
+                    </nav>
                 </div>
-                <hr className="border-gray-200" />
+                <hr className="border-border" />
                  <div className="flex justify-between items-center">
                     <div className="flex items-center gap-8">
                         <Link href="/" className="w-35 h-5  relative">
@@ -53,8 +53,51 @@ export function Footer () {
 
                 
 
-            <div className="flex lg:hidden">
-                
+            <div className="flex flex-col gap-6 items-center">
+                 <Link href="/" className="w-35 h-5 relative">
+                    <Image 
+                    src="/images/Logo-dark.svg"
+                    alt="/"
+                    fill
+                    className="object-contain"></Image>
+                </Link>
+
+                <nav className="flex flex-col gap-6 text-center">
+                    <Link href="/personnel" className=" hover:text-gray-600">Кадры</Link>
+                    <Link href="/startups" className=" hover:text-gray-600">Стартапы</Link>
+                    <Link href="/contacts" className=" hover:text-gray-600">Контакты</Link>
+                </nav>
+
+                <hr  className="border-border"/>
+
+                <nav className="flex flex-col gap-6  text-center">
+                    <Link href="/" className="hover:text-gray-600">Партнер</Link>
+                    <Link href="/" className="hover:text-gray-600">Партнер</Link>
+                    <Link href="/" className="hover:text-gray-600">Партнер</Link>
+                    <Link href="/" className="hover:text-gray-600">Партнер</Link>
+                </nav>
+
+                <div className=" flex gap-4 items-center justify-between w-full">
+                    <div>
+                        <p className="">TeamNest@bk.ru</p>
+                        <Link href="/" className="size-8 relative">
+                            <Image
+                            src="/images/icons/VK.svg"
+                            alt="vk"
+                            fill
+                            ></Image>
+                        </Link>
+                        <Link href="/" className="size-8 relative">
+                            <Image
+                            src="/images/icons/Telegram.svg"
+                            alt="telegram"
+                            fill
+                            ></Image>
+                        </Link>
+                    </div>
+
+                     <p>©2026</p>
+                </div>
             </div>
         </footer>
     )
